099a3ecd2256bd5a2a9a26ac7b17dc0a8c5ad8f9
deveshtyagi735 <tyagidevesh809@gmail.com>
2026-04-22 22:57:07 +0530

    all set

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: "https://ai-frontend-snowy.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
 }))
 
 /* require all the routes here */
